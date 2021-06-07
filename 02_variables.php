d77cd05417d24a0cec80e5b91fdb00d02ba96a17
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-07 11:18:35 -0700

    Check types of variables

--- 02_variables.php
+++ 02_variables.php
@@ -28,6 +28,7 @@ echo $name."<br>", $age; // Dot is concatenating tags
 
 // Print types of the variables
 echo "<br>";
+echo "<br>";
 echo "Type of the variable 'name' is ".gettype($name).'<br>';
 echo "Type of the variable 'age' is ".gettype($age).'<br>';
 echo "Type of the variable 'isMale' is ".gettype($isMale).'<br>';
@@ -35,15 +36,35 @@ echo "Type of the variable 'height' is ".gettype($height).'<br>';
 echo "Type of the variable 'salary' is ".gettype($salary).'<br>';
 
 // Print the whole variable
+echo "<br>";
+echo "<br>";
+var_dump($name);
 
 // Change the value of the variable
+echo "<br>";
+echo "<br>";
+$name = "Anna";
+echo $name;
 
 // Print type of the variable
+echo "<br>";
+echo "<br>";
+echo gettype($name);
 
 // Variable checking functions
+echo "<br>";
+echo "<br>";
+is_string($name);
 
 // Check if variable is defined
+isset($name);
 
 // Constants
+echo "<br>";
+echo "<br>";
+define('PI', 3.14);
+echo PI."<br>";
 
 // Using PHP built-in constants
+echo SORT_ASC."<br>";
+echo PHP_INT_MAX."<br>";
