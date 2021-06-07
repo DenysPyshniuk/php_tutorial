198cca224496393c1bc6ebab66826593c5db6aa2
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-07 10:52:31 -0700

    Variables, print and type of the variables

--- 02_variables.php
+++ 02_variables.php
@@ -2,13 +2,37 @@
 
 // What is a variable
 
-// Variable types
+/*
+--Variable types:
+-String
+-Integer
+-Float/Double
+-Boolean
+-Null
+-Array
+-Object
+-Resource
+*/
+
 
 // Declare variables
+$name = "Denys";
+$age = 30;
+$isMale = true;
+$height = 1.87;
+$salary = null;
+
 
 // Print the variables. Explain what is concatenation
+echo $name."<br>", $age; // Dot is concatenating tags
 
 // Print types of the variables
+echo "<br>";
+echo "Type of the variable 'name' is ".gettype($name).'<br>';
+echo "Type of the variable 'age' is ".gettype($age).'<br>';
+echo "Type of the variable 'isMale' is ".gettype($isMale).'<br>';
+echo "Type of the variable 'height' is ".gettype($height).'<br>';
+echo "Type of the variable 'salary' is ".gettype($salary).'<br>';
 
 // Print the whole variable
 
