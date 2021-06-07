3aac6f91f974af6b5f10838387c5ee23dd81b2cf
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-07 11:31:32 -0700

    Arithmetic operators

--- 03_numbers.php
+++ 03_numbers.php
@@ -1,10 +1,29 @@
 <?php
 
 // Declaring numbers
+$a = 10;
+$b = 4;
+$c = 1.2;
 
 // Arithmetic operations
+echo $a + $b * $c."<br>";
+echo "<br>";
+echo $a % $b;
+echo "<br>";
 
 // Assignment with math operators
+$a = 10;
+$b = 4;
+$a = $a + $b;
+echo $a;
+echo "<br>";
+// Same as:
+$a = 10;
+$b = 4;
+$a += $b;
+echo $a;
+echo "<br>";
+
 
 // Increment operator
 
