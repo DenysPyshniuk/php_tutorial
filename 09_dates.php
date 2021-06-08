64a7353e972d8a846fd67e94434fe3ca2765f3f4
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-08 11:46:17 -0700

    Dates in php

--- 09_dates.php
+++ 09_dates.php
@@ -1,13 +1,27 @@
 <?php
 
 // Print current date
+echo date("Y-m-d H:i:s");
+echo "<br>";
 
 // Print yesterday
+echo date("Y-m-d H:i:s", time() - 60*60*24);
+echo "<br>";
 
 // Different format: https://www.php.net/manual/en/function.date.php
-
-// Print current timestamp
-
+echo date("F j Y, H:i:s");
+echo "<br>";
+// Print current timestamp EPOCH STAMP in seconds since 1970
+echo time()."<br>";
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
-
+$parsedDate = date_parse("2020-10-12 09:00:00");
+echo "<pre>";
+var_dump($parsedDate);
+echo "</pre>";
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+$dateString = 'February 4 2020 12:45:35';
+
+$parsedDate = date_parse_from_format('F j Y H:i:s', $dateString);
+echo '<pre>';
+var_dump($parsedDate);
+echo '</pre>';
