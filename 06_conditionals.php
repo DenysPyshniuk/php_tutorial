561963cabdad855802eaa9ab68e8ff4a33bb32da
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-08 10:52:20 -0700

    Conditionals in php

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -4,21 +4,54 @@ $age = 20;
 $salary = 300000;
 
 // Sample if
+if($age === 20) {
+  echo "true";
+}
 
 // Without circle braces
-
+if($age === 20)echo "true";
 // Sample if-else
-
+if($age > 20)echo "it is";
+else echo "it is as well";
 // Difference between == and ===
 
 // if AND
+$age == 20; // true
+$age == '20'; // true
 
+$age === 20; // true
+$age === '20'; // false
 // if OR
+if ($age > 20 && $salary === 300000) {
 
+}
+// if OR
+if ($age > 20 OR $salary === 300000) {
+
+}
+echo '<br>';
 // Ternary if
-
+echo $age < 22 ? 'Young' : 'Old';
+echo '<br>';
 // Short ternary
-
+$myAge = $age ?: 18; // Equivalent of "$age ? $age : 18"
 // Null coalescing operator
-
+$var = isset($name) ? $name : 'John';
+$var = $name ?? 'John'; // Equivalent of above
+echo $var.'<br>';
 // switch
+$userRole = 'admin'; // admin, editor, user
+
+switch ($userRole) {
+    case 'admin':
+        echo 'You can do anything<br>';
+        break;
+    case 'editor';
+        echo 'You can edit content<br>';
+        break;
+    case 'user':
+        echo 'You can view posts and comment<br>';
+        break;
+    default:
+        echo 'Unknown role<br>';
+}
