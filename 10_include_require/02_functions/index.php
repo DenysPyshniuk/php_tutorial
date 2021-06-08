de74de7733af409631f90dad88007ee2827ee861
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-08 13:06:00 -0700

    Require math.php with functions

--- 10_include_require/02_functions/index.php
+++ 10_include_require/02_functions/index.php
@@ -1,1 +1,7 @@
 <?php
+
+require_once "math.php";
+
+echo "sum is: ".add(10, 6)."<br>";
+echo "substract is: ".subtract(10, 4)."<br>";
+echo "Guess what was the numbers that we were manipulating with?";
