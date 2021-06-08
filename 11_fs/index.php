b6e93b90c664d9fa34fdaf0a005bea7ef5ad3697
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-08 14:35:39 -0700

    File system commands in php

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,16 +1,25 @@
 <?php
 // Magic constants
-
+echo __DIR__.'<br>';
+echo __FILE__.'<br>';
+echo __LINE__.'<br>';
 // Create directory
-
+// mkdir("test");
 // Rename directory
-
+rename("/1.txt", "/2.txt");
 // Delete directory
-
+// rmdir("test2");
 // Read files and folders inside directory
+echo file_get_contents("lorem.txt");
 
+$files = scandir('./');
+echo "<pre>";
+var_dump($files);
+echo "</pre>";
 // file_get_contents, file_put_contents
-
+echo file_get_contents("lorem.txt");
+file_put_contents("sample.txt", "iwhqiguhwighuweprghweiprguhreiowguhiouewruhgihewriguherwiuhgiurewhgiuhweriopgwerotherwpi");
+echo file_get_contents("sample.txt");
 // file_get_contents from URL
 
 // https://www.php.net/manual/en/book.filesystem.php
