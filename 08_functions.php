4665c3e9f1ed11ff592e8fccf0b32f9b960ac429
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-08 11:37:04 -0700

    Functions in php

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,37 @@
 <?php
 
 // Function which prints "Hello I am Zura"
+// function hello(){
+//   echo "Hello I am Denys";
+// }
+// hello();
 
 // Function with argument
-
+function hello($name){
+  echo "Hello I am $name";
+}
+hello("Denys");
 // Create sum of two functions
+echo "<br>";
+
+// function sum($a, $b){
+//   return $a + $b;
+// }
+// echo sum(5, 3);
 
 // Create function to sum all numbers using ...$nums
+// function sum(...$nums){
+//   $sum = 0;
+//   foreach($nums as $num){
+//     $sum += $num;
+//   }
+//   return $sum;
+// }
+// echo sum(5, 3, 3);
 
 // Arrow functions
+function sum(...$nums)
+{
+  return array_reduce($nums, fn($carry, $n) => $carry + $n);
+}
+echo sum(1, 2, 3, 4, 6);
