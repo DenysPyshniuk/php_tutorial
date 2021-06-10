f3ce2191b98216505175ed092c66bfe3cf92eb23
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-09 22:30:46 -0700

    Give Permition to write

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,25 +1,30 @@
 <?php
+//Code to display any errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Magic constants
 echo __DIR__.'<br>';
 echo __FILE__.'<br>';
 echo __LINE__.'<br>';
 // Create directory
-// mkdir("test");
+// mkdir('test1', 0700);
 // Rename directory
-rename("/1.txt", "/2.txt");
+// rename("1.txt", "2.txt");
 // Delete directory
-// rmdir("test2");
+// rmdir("test1");
 // Read files and folders inside directory
-echo file_get_contents("lorem.txt");
+// echo file_get_contents("lorem.txt");
 
-$files = scandir('./');
-echo "<pre>";
-var_dump($files);
-echo "</pre>";
+// $files = scandir('./');
+// echo "<pre>";
+// var_dump($files);
+// echo "</pre>";
 // file_get_contents, file_put_contents
-echo file_get_contents("lorem.txt");
-file_put_contents("sample.txt", "iwhqiguhwighuweprghweiprguhreiowguhiouewruhgihewriguherwiuhgiurewhgiuhweriopgwerotherwpi");
-echo file_get_contents("sample.txt");
+// echo file_get_contents("lorem.txt");
+// file_put_contents("sample.txt", "iwhqiguhwighuweprghweiprguhreiowguhiouewruhgihewriguherwiuhgiurewhgiuhweriopgwerotherwpi");
+// echo file_get_contents("sample.txt");
 // file_get_contents from URL
 
 // https://www.php.net/manual/en/book.filesystem.php
