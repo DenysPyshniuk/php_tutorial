53c2417f01e854e16b8c570267f87e2d50be46dd
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-14 11:37:33 -0700

    Require database partial

--- 14_product_crud/02_better_version/index.php
+++ 14_product_crud/02_better_version/index.php
@@ -4,8 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-  $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once "database.php";
 
 
   $search = $_GET['search'] ?? '';
