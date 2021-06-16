ab70bd9fe201f0a5889e25bf4425a7b1992314e9
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-16 12:27:08 -0700

    Clean code

--- 14_product_crud/03_good_version/Router.php
+++ 14_product_crud/03_good_version/Router.php
@@ -2,50 +2,53 @@
 
 namespace app;
 
-class Router {
-  public array $getRoutes = [];
-  public array $postRoutes = [];
-  public Database $db;
+class Router
+{
+    public array $getRoutes = [];
+    public array $postRoutes = [];
 
-  public function __construct()
-  {
-    $this->db = new Database();
-  }
+    public ?Database $database = null;
 
-
-  public function get($url, $fn){
-    $this->getRoutes[$url] = $fn;
-  }
-
-  public function post($url, $fn){
-    $this->postRoutes[$url] = $fn;
-  }
-
-  public function resolve(){
-    $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-    $method = $_SERVER['REQUEST_METHOD'];
-
-    if ($method == 'GET') {
-      $fn = $this->getRoutes[$currentUrl] ?? null;
-    } else {
-      $fn = $this->postRoutes[$currentUrl] ?? null;
-    }
-    if ($fn) {
-      call_user_func($fn, $this);
-    } else {
-      echo "Page not found";
-    }
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
     }
 
-    public function renderView($view, $params = []){
-      foreach ($params as $key => $value) {
-        $$key = $value;
-
-      }
-      ob_start();
-      include_once __DIR__."/views/$view.php";
-      $content = ob_get_clean();
-      include_once __DIR__.'/views/_layout.php';
+    public function get($url, $fn)
+    {
+        $this->getRoutes[$url] = $fn;
     }
 
+    public function post($url, $fn)
+    {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function resolve()
+    {
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $url = $_SERVER['PATH_INFO'] ?? '/';
+
+        if ($method === 'get') {
+            $fn = $this->getRoutes[$url] ?? null;
+        } else {
+            $fn = $this->postRoutes[$url] ?? null;
+        }
+        if (!$fn) {
+            echo 'Page not found';
+            exit;
+        }
+        echo call_user_func($fn, $this);
+    }
+
+    public function renderView($view, $params = [])
+    {
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
+        ob_start();
+        include __DIR__."/views/$view.php";
+        $content = ob_get_clean();
+        include __DIR__."/views/_layout.php";
+    }
 }
