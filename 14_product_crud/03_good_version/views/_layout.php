6f134f87eeb187387c26923bc2cf912aeb09e503
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-06-16 12:30:10 -0700

    Refactor code

--- 14_product_crud/03_good_version/views/_layout.php
+++ 14_product_crud/03_good_version/views/_layout.php
@@ -1,7 +1,5 @@
-
 <!doctype html>
 <html lang="en">
-
 <head>
   <!-- Required meta tags -->
   <meta charset="utf-8">
@@ -10,12 +8,11 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="/app.css">
-
   <title>Products CRUD</title>
 </head>
 <body>
 
-<?php echo $content; ?>
+<?php echo $content ?>
 
 </body>
 </html>
--- 14_product_crud/03_good_version/views/products/index.php
+++ 14_product_crud/03_good_version/views/products/index.php
@@ -1,47 +1,47 @@
-<h1>Product List</h1>
+<h1>Product CRUD</h1>
 
 <p>
-      <a href="/products/create" class="btn btn-success">Create Product</a>
-    </p>
-
-<form>
-<div class="input-group mb-3">
-  <input type="text" class="form-control" placeholder="Search for products" name="search" value="<?php echo $search ?>">
-  <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
-</div></form>
-
+    <a href="/products/create" type="button" class="btn btn-sm btn-success">Add Product</a>
+</p>
+<form action="" method="get">
+    <div class="input-group mb-3">
+      <input type="text" name="search" class="form-control" placeholder="Search" value="<?php echo $keyword ?>">
+      <div class="input-group-append">
+        <button class="btn btn-success" type="submit">Search</button>
+      </div>
+    </div>
+</form>
 <table class="table">
-  <thead>
+    <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">Image</th>
-      <th scope="col">Title</th>
-      <th scope="col">Description</th>
-      <th scope="col">Price</th>
-      <th scope="col">Create Date</th>
-      <th scope="col">Action</th>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+        <th scope="col">Actions</th>
     </tr>
-  </thead>
-  <tbody>
-  <?php foreach($products as $i => $product): ?>
-      <tr>
-      <th scope="row"><?php echo $i + 1; ?></th>
-      <td>
-        <img src="/<?php echo $product['image'] ?>" class="thumb-image">
-      </td>
-      <td><?php echo $product['title']; ?></td>
-      <td><?php echo $product['description']; ?></td>
-
-      <td><?php echo $product['price']; ?></td>
-      <td><?php echo $product['create_date']; ?></td>
-      <td>
-      <a href="update.php?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
-      <form style="display: inline-block;" method="POST" action="delete.php">
-      <input type="hidden" name="id" value="<?php echo $product['id'] ?>">
-      <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
-      </form>
-      </td>
-    </tr>
-  <?php endforeach; ?>
-  </tbody>
+    </thead>
+    <tbody>
+    <?php foreach ($products as $i => $product) { ?>
+        <tr>
+            <th scope="row"><?php echo $i + 1 ?></th>
+            <td>
+                <?php if ($product['image']): ?>
+                    <img src="/<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>" class="product-img">
+                <?php endif; ?>
+            </td>
+            <td><?php echo $product['title'] ?></td>
+            <td><?php echo $product['price'] ?></td>
+            <td><?php echo $product['create_date'] ?></td>
+            <td>
+                <a href="/products/update?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                <form method="post" action="/products/delete" style="display: inline-block">
+                    <input  type="hidden" name="id" value="<?php echo $product['id'] ?>"/>
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
 </table>
